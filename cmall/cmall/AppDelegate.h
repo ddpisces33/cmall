b1b8fa29c6ecf61b67d9d63f501437516412cf0a
杜澎 <dupeng@iiMacBook-Pro.local>
2015-07-30 20:05:09 +0800

    new project

---
+++ cmall/cmall/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cmall
+//
+//  Created by 杜澎 on 15/7/30.
+//  Copyright (c) 2015年 Picnic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cmall/cmall/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cmall
+//
+//  Created by 杜澎 on 15/7/30.
+//  Copyright (c) 2015年 Picnic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
